b916f82d298fd65e11f83545cc3f38d9a1f652b0
elisaveta96 <37802625+elisaveta96@users.noreply.github.com>
2018-04-01 22:05:01 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,11 @@
-
+x <- c(4, 1, 1, 4)
+y <- c(1, 4)
+x - y
+#
+s <- c(x, y)
+rep(s, 10)
+length(s)
+rep(s, each = 3)
+seq(7, 21, by=1)
+7:21
+length(7:21)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,27 @@
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2) 
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+xmax - xmin          
+avgxmin <- mean(xmin)
+avgxmin
+avgxmax<- mean(xmax)
+avgxmax
+xmin [xmin < avgxmin]
+xmax [xmax < avgxmax]
+names(xmin) = c("03Monday18", "04Tuesday18", "05Wednesday18", "04Thursday18", "05Friday18", "06Saturday18", "07Sunday18") 
+xmin
+names(xmax) = c("03Monday18", "04Tuesday18", "05Wednesday18", "04Thursday18", "05Friday18", "06Saturday18", "07Sunday18") 
+xmax
+temperatures = data.frame (xmin, xmax)
+data.frame (xmin, xmax)
+xminFahrenheit <- c(xmin * 1.8 +32)
+xminFahrenheit
+temperatures = data.frame (xmin, xmax, xminFahrenheit)
+data.frame (xmin, xmax, xminFahrenheit)
+xmaxFahrenheit <- c(xmax * 1.8 +32)
+xmaxFahrenheit
+temperatures = data.frame (xmin, xmax, xminFahrenheit, xmaxFahrenheit)
+data.frame (xmin, xmax, xminFahrenheit, xmaxFahrenheit)
+temperaturesFahrenheit = data.frame (xminFahrenheit, xmaxFahrenheit)
+data.frame (xminFahrenheit, xmaxFahrenheit)
+
 
